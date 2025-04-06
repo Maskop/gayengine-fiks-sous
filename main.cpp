676ab78ae44bc6b1da954791bdfc1cb58330f152
GayStation <GayStation@gay.cvut.fiks.net.eng.telnet.iiyama.cz>
2025-04-06 12:35:46 +0200

    aaaaaa

--- main.cpp
+++ main.cpp
@@ -4,18 +4,30 @@
 #define otherwise else
 #define ShallTheeWindowClose WindowShouldClose
 
+// you shall not change them
 const int height = 350;
 const int width = 350;
-const int targetFPS = 69;
+const int targetFPS = 1000000000;
+
+// Not a constat just because
+float gAcc = 12;
 
 int main() {
     InitWindow(height, width, "There shall be a window");
     SetTargetFPS(targetFPS);
+    
 
-    while (!WindowShouldClose()){
+    int startPosX = 69;
+    int startPosY = 69;
+    while (!ShallTheeWindowClose()){
         BeginDrawing();
-        DrawRectangle(69, 69, 69, 69, RED);
+        ClearBackground(YELLOW);
+        DrawRectangle(startPosX, startPosY, 69, 69, RED);
+        DrawFPS(10, 10);
         EndDrawing();
+        if (IsKeyDown(KEY_W)) {
+            startPosY++; startPosX++;
+        }
     }
     return 0;
 }
