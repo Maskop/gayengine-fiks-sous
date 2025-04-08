0f70683a2c7726cb77e844a420cdb68cfd23432d
GayStation <GayStation@gay.cvut.fiks.net.eng.telnet.iiyama.cz>
2025-04-08 15:04:11 +0200

    Cervena kocicka

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <functional>
+#include <optional>
 
 using namespace std;
 
@@ -47,6 +48,29 @@ Rectangle keyMove(Rectangle ctverec, Vector2 vec, float speed, int key, const ve
     return ctverec;
 }
 
+
+class RecObj{
+public:
+    RecObj(Rectangle rec, Color color, std::optional<Texture2D> image): rec(rec), image(image), color(color){
+    };
+    void draw(){
+        DrawRectangleRec(rec, color);
+        if (image.has_value()){
+            DrawTexture(image.value(), rec.x, rec.y, color);
+        }
+    }
+    void move(Rectangle newPos){
+        rec = newPos;
+    }
+    const Rectangle & getRec(){
+        return rec;
+    }
+private:
+    Rectangle rec;
+    Color color;
+    std::optional<Texture2D> image;
+};
+
 class Button{
 public:
     Button(Rectangle rec, string text, function<void()> callback): rec(rec), text(text), callback(callback){
@@ -78,15 +102,16 @@ int main() {
     float startPosY = 69;
     Color color_green = GREEN;
     Color color_yellow = YELLOW;
-    Rectangle ctverecek{69,69,69,69};
+    auto kosiska = LoadTexture("kocicka.jpg");
+    RecObj ctverecek(Rectangle{69,69,kosiska.width, kosiska.height}, RED, kosiska);
     vector bounderies{Rectangle{-10, 0, 10, height}, Rectangle{width, 0, 10, height}, Rectangle{0, -10, width, 10}, Rectangle{0, height, width, 10}
     };
     Button tlacitko(Rectangle{100, 100, 50, 20}, "Ahoj", [&](){background_color = color_green;});
-    LoadImage("kocicka.jpg");
     until (ShallTheeWindowClose()){
         BeginDrawing();
         ClearBackground(background_color);
-        DrawRectangleRec(ctverecek, RED);
+        DrawTexture(kosiska, 0, 0, PURPLE);
+        ctverecek.draw();
         tlacitko.draw();
         if (tlacitko.checkButtonPress()) {
             swap(color_green, color_yellow);
@@ -95,10 +120,10 @@ int main() {
             DrawRectangleRec(r, GREEN);
         }
         DrawFPS(10, 10);
-            ctverecek = keyMove(ctverecek, {0, -1}, speed, KEY_W, bounderies);
-            ctverecek = keyMove(ctverecek, {0, 1}, speed, KEY_S, bounderies);
-            ctverecek = keyMove(ctverecek, {1, 0}, speed, KEY_D, bounderies);
-            ctverecek = keyMove(ctverecek, {-1, 0}, speed, KEY_A, bounderies);
+            ctverecek.move(keyMove(ctverecek.getRec(), {0, -1}, speed, KEY_W, bounderies));
+            ctverecek.move(keyMove(ctverecek.getRec(), {0, 1}, speed, KEY_S, bounderies));
+            ctverecek.move(keyMove(ctverecek.getRec(), {1, 0}, speed, KEY_D, bounderies));
+            ctverecek.move(keyMove(ctverecek.getRec(), {-1, 0}, speed, KEY_A, bounderies));
         EndDrawing();
     }
     return 0;
