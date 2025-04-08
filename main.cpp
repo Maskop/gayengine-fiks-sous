00e6c1a28d6a5253f216587ac833a386fb152dad
Bertik23 <bertikxxiii@gmail.com>
2025-04-08 15:31:50 +0200

    nice button

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include "raylib/include/raylib.h"
 #include <chrono>
 #include <ctime>
-#include <iostream>
-#include <vector>
-#include <string>
 #include <functional>
+#include <iostream>
 #include <optional>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 #define inTheCaseOf if
 #define otherwise else
 #define thyActionRepeatsInTheCaseOf while
-#define until(arg) while(!(arg))
+#define until(arg) while (!(arg))
 #define ShallTheeWindowClose WindowShouldClose
 
 // you shall not change them
@@ -23,71 +23,86 @@ const int speed = 240;
 // Not a constat just because
 float gAcc = 12;
 
-Rectangle moveRec(Rectangle rec, const Vector2 & vec){
+std::pair<int, int> getBestFontSizeToFit(const char *text, int maxWidth,
+                                         int maxHeight) {
+    int fontSize = 1;
+    int textWidth;
+    while (true) {
+        textWidth = MeasureText(text, fontSize);
+        int textHeight = fontSize; // Roughly the font height
+        if (textWidth > maxWidth || textHeight > maxHeight)
+            break;
+        fontSize++;
+    }
+    return {fontSize - 1, textWidth}; // Last fitting size
+}
+
+Rectangle moveRec(Rectangle rec, const Vector2 &vec) {
     rec.x += vec.x;
     rec.y += vec.y;
     return rec;
 }
 
-Vector2 operator*(Vector2 v, float a){
+Vector2 operator*(Vector2 v, float a) {
     v.x *= a;
     v.y *= a;
     return v;
 }
 
-Rectangle keyMove(Rectangle ctverec, Vector2 vec, float speed, int key, const vector<Rectangle> & bounderies) {
-    inTheCaseOf (IsKeyDown(key)) {
+Rectangle keyMove(Rectangle ctverec, Vector2 vec, float speed, int key,
+                  const vector<Rectangle> &bounderies) {
+    inTheCaseOf(IsKeyDown(key)) {
         auto newPos = moveRec(ctverec, vec * GetFrameTime() * speed);
         bool canMove = true;
-        for (const auto & r : bounderies){
+        for (const auto &r : bounderies) {
             canMove = canMove && !CheckCollisionRecs(newPos, r);
         }
         if (canMove)
-            ctverec = newPos; 
-        }
+            ctverec = newPos;
+    }
     return ctverec;
 }
 
-
-class RecObj{
-public:
-    RecObj(Rectangle rec, Color color, std::optional<Texture2D> image): rec(rec), image(image), color(color){
-    };
-    void draw(){
+class RecObj {
+  public:
+    RecObj(Rectangle rec, Color color, std::optional<Texture2D> image)
+        : rec(rec), color(color), image(image) {};
+    void draw() {
         DrawRectangleRec(rec, color);
-        if (image.has_value()){
+        if (image.has_value()) {
             DrawTexture(image.value(), rec.x, rec.y, color);
         }
     }
-    void move(Rectangle newPos){
-        rec = newPos;
-    }
-    const Rectangle & getRec(){
-        return rec;
-    }
-private:
+    void move(Rectangle newPos) { rec = newPos; }
+    const Rectangle &getRec() { return rec; }
+
+  private:
     Rectangle rec;
     Color color;
     std::optional<Texture2D> image;
 };
 
-class Button{
-public:
-    Button(Rectangle rec, string text, function<void()> callback): rec(rec), text(text), callback(callback){
-    };
+class Button {
+  public:
+    Button(Rectangle rec, string text, function<void()> callback)
+        : rec(rec), text(text), callback(callback) {};
     void draw() {
         DrawRectangleRec(rec, WHITE);
-        DrawText(text.c_str(), rec.x, rec.y, 12, BLACK);
+        auto t
+            = getBestFontSizeToFit(text.c_str(), rec.width - 2, rec.height - 2);
+        DrawText(text.c_str(), rec.x + (rec.width - t.second) / 2, rec.y + 1,
+                 t.first, BLACK);
     }
-    bool checkButtonPress(){
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), rec)){
+    bool checkButtonPress() {
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)
+            && CheckCollisionPointRec(GetMousePosition(), rec)) {
             callback();
             return true;
         }
         return false;
     }
 
-private:
+  private:
     Rectangle rec;
     string text;
     function<void()> callback;
@@ -96,18 +111,21 @@ private:
 int main() {
     InitWindow(width, height, "There shall be a window");
     SetTargetFPS(targetFPS);
-    
+
     Color background_color = YELLOW;
     float startPosX = 69;
     float startPosY = 69;
     Color color_green = GREEN;
     Color color_yellow = YELLOW;
     auto kosiska = LoadTexture("kocicka.jpg");
-    RecObj ctverecek(Rectangle{69,69,kosiska.width, kosiska.height}, RED, kosiska);
-    vector bounderies{Rectangle{-10, 0, 10, height}, Rectangle{width, 0, 10, height}, Rectangle{0, -10, width, 10}, Rectangle{0, height, width, 10}
-    };
-    Button tlacitko(Rectangle{100, 100, 50, 20}, "Ahoj", [&](){background_color = color_green;});
-    until (ShallTheeWindowClose()){
+    RecObj ctverecek(Rectangle{69, 69, kosiska.width, kosiska.height}, RED,
+                     kosiska);
+    vector bounderies{
+        Rectangle{-10, 0, 10, height}, Rectangle{width, 0, 10, height},
+        Rectangle{0, -10, width, 10}, Rectangle{0, height, width, 10}};
+    Button tlacitko(Rectangle{100, 100, 50, 20}, "Ahoj",
+                    [&]() { background_color = color_green; });
+    until(ShallTheeWindowClose()) {
         BeginDrawing();
         ClearBackground(background_color);
         DrawTexture(kosiska, 0, 0, PURPLE);
@@ -116,14 +134,18 @@ int main() {
         if (tlacitko.checkButtonPress()) {
             swap(color_green, color_yellow);
         }
-        for (const auto & r : bounderies){
+        for (const auto &r : bounderies) {
             DrawRectangleRec(r, GREEN);
         }
         DrawFPS(10, 10);
-            ctverecek.move(keyMove(ctverecek.getRec(), {0, -1}, speed, KEY_W, bounderies));
-            ctverecek.move(keyMove(ctverecek.getRec(), {0, 1}, speed, KEY_S, bounderies));
-            ctverecek.move(keyMove(ctverecek.getRec(), {1, 0}, speed, KEY_D, bounderies));
-            ctverecek.move(keyMove(ctverecek.getRec(), {-1, 0}, speed, KEY_A, bounderies));
+        ctverecek.move(
+            keyMove(ctverecek.getRec(), {0, -1}, speed, KEY_W, bounderies));
+        ctverecek.move(
+            keyMove(ctverecek.getRec(), {0, 1}, speed, KEY_S, bounderies));
+        ctverecek.move(
+            keyMove(ctverecek.getRec(), {1, 0}, speed, KEY_D, bounderies));
+        ctverecek.move(
+            keyMove(ctverecek.getRec(), {-1, 0}, speed, KEY_A, bounderies));
         EndDrawing();
     }
     return 0;
