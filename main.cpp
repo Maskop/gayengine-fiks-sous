84c792370e39d1f0134feecdc31f15aa936bb959
maskop <jachym.otahal@gmail.com>
2025-04-09 04:51:05 +0200

    mannnn

--- main.cpp
+++ main.cpp
@@ -88,6 +88,26 @@ class RecObj : public Interactable {
     Color color;
     std::optional<Texture2D> image;
     vector<char> collidableSides;
+    unsigned char checkSide(RecObj other) {
+        inTheCaseOf(this->rec.x + this->rec.height
+                    >= other.getRec().x) { // Under
+            return 3;
+        }
+        butIf(this->rec.x
+              <= other.getRec().x + other.getRec().height) { // Above
+            return 1;
+        }
+        butIf(this->rec.y + this->rec.width
+              <= other.getRec().y) { // To the left
+            return 0;
+        }
+        butIf(this->rec.y
+              <= other.getRec().y + other.getRec().width) { // To the right
+            return 2;
+        }
+        otherwise { return 4; }
+        return 5;
+    }
 };
 
 vector<RecObj> interactableObjects;
